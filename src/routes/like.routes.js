29e6d7dc28974518abea4fedfb4aa72a795264a7
sathvic-poudala <sathvicpoudala@gmail.com>
2026-06-13 16:35:05 +0530

    Refactor like routes: update endpoint paths to include 'toggle' for better clarity

--- src/routes/like.routes.js
+++ src/routes/like.routes.js
@@ -7,9 +7,9 @@ const router = Router()
 app.use(verifyJWT)//all of these routes require verifyjwt
 
 //routes
-router.route("/video/:videoId").post(toggleVideoLike)
-router.route("/comment/:commentId").post(toggleCommentLike)
-router.route("/tweet/:tweetId").post(toggleTweetLike)
+router.route("/toggle/video/:videoId").post(toggleVideoLike)
+router.route("/toggle/comment/:commentId").post(toggleCommentLike)
+router.route("/toggle/tweet/:tweetId").post(toggleTweetLike)
 
 
 export default router
