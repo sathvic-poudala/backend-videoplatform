3f23c3a303b7b1a4db8dcff417835ed416c82967
sathvic-poudala <sathvicpoudala@gmail.com>
2026-06-19 12:55:04 +0530

    Add getUserPlaylists route: implement retrieval of playlists created by a user

--- src/controllers/playlist.controller.js
+++ src/controllers/playlist.controller.js
@@ -3,7 +3,7 @@ import { Playlist } from "../models/playlist.model";
 import { ApiError } from "../utils/ApiError";
 import { ApiResponse } from "../utils/ApiResponse";
 
-const createPlaylist = asyncHandler(async (req, res) => {
+const createPlaylist = asyncHandler(async(req, res) => {
     const { name, description = "" } = req.body;
     if(!name) {
         throw new ApiError(400,"name is required")
@@ -30,3 +30,30 @@ const createPlaylist = asyncHandler(async (req, res) => {
     )
 })
 
+const getUserPlaylists = asyncHandler(async(req, res) => {
+    const {userId} = req.params
+
+    const playlists = await Playlist.find({ createdBy: userId });
+
+    if(!playlists) {
+        throw new ApiError(400, "user has no playlist");
+        
+    }
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(
+            200,
+            "found users playlists",
+            playlists
+        )
+    )
+})
+
+
+
+export { 
+    createPlaylist, 
+    getUserPlaylists
+ }
