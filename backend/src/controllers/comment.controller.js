8624084c56edb9a06bae7b5eca31d6e5486aafbe
sathvic-poudala <sathvicpoudala@gmail.com>
2026-07-03 20:59:50 +0530

    feat: import Video model in comment controller for comment creation functionality

--- backend/src/controllers/comment.controller.js
+++ backend/src/controllers/comment.controller.js
@@ -4,6 +4,7 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { User } from "../models/user.model.js";
+import { Video } from "../models/video.model.js";
 
 const createComment = async(videoId, userId, content) => {
     const comment = await Comment.create({
