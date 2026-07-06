7adb7e637460d8bff7bb9baffd301182c20a9176
sathvic-poudala <sathvicpoudala@gmail.com>
2026-07-06 11:42:40 +0530

    refactor: add validation middleware for tweet routes to enhance request handling

--- backend/src/routes/tweet.routes.js
+++ backend/src/routes/tweet.routes.js
@@ -1,12 +1,17 @@
 import { Router } from 'express'
 import {verifyJWT} from "../middlewares/auth.middleware.js"
 import { createTweet, deleteTweet, getUserTweets, updateTweet } from '../controllers/tweet.controller.js';
+import { validateObjectId, validateBodyFields } from "../middlewares/validate.middleware.js";
 
 const router = Router();
 router.use(verifyJWT);
 
-router.route("/").post(createTweet);
-router.route("/user/:userId").get(getUserTweets);
-router.route("/:tweetId").patch(updateTweet).delete(deleteTweet)
+router.route("/").post(validateBodyFields(["content"]), createTweet);
+
+router.route("/user/:userId").get(validateObjectId("userId"), getUserTweets);
+
+router.route("/:tweetId")
+    .patch(validateObjectId("tweetId"), validateBodyFields(["content"]), updateTweet)
+    .delete(validateObjectId("tweetId"), deleteTweet)
 
 export default router
